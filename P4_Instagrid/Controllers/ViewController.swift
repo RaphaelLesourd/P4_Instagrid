d219d4793137a35607cfa622ff74e25d508f5f44
Birkyboy <birkyboy@icloud.com>
2021-03-21 18:53:03 +0100

    addition of comment for functions

--- P4_Instagrid/Controllers/ViewController.swift
+++ P4_Instagrid/Controllers/ViewController.swift
@@ -10,13 +10,12 @@ import Photos
 
 class ViewController: UIViewController, UIGestureRecognizerDelegate {
     
-    
     @IBOutlet weak var imageGridContainerView: UIView!
     @IBOutlet weak var topImageStackView: UIStackView!
     @IBOutlet weak var bottomImageStackView: UIStackView!
-    
-    @IBOutlet var imageButtons: [UIButton]!
-    @IBOutlet var controlButtons: [UIButton]!
+   
+    @IBOutlet var imageButtonsArray: [UIButton]!
+    @IBOutlet var controlButtonsArray: [UIButton]!
     
     @IBOutlet weak var swipeIcon: UIImageView!
     @IBOutlet weak var swipeLabel: UILabel!
@@ -37,23 +36,30 @@ class ViewController: UIViewController, UIGestureRecognizerDelegate {
         gestureSetup()
         
         self.imagePicker = ImagePicker(presentationController: self, delegate: self)
-        controlButtonsAction(controlButtons[0])
+        controlButtonsAction(controlButtonsArray[0])
         
-        NotificationCenter.default.addObserver(self, selector: #selector(directionChange), name: UIDevice.orientationDidChangeNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(updateUiOnOrientationChange), name: UIDevice.orientationDidChangeNotification, object: nil)
         
     }
     
     // MARK: - Setup
     
+    /// Sets up all the the Buttons
     private func buttonsSetup() {
         
-        for button in imageButtons {
+        /// for loop assigning the same empty state image to all 4 buttons
+        /// set contentmode to keep image proportions and fill the button
+        /// assign same taget function to all 4 buttons
+        for button in imageButtonsArray {
             button.imageView?.contentMode = .scaleAspectFill
             button.setImage(emptyStateImageButton, for: .normal)
             button.addTarget(self, action: #selector(imageButtonAction(_:)), for: .touchUpInside)
         }
         
-        for button in controlButtons {
+        /// for loop assigning the same selected state image to all 3 coltrol buttons
+        /// set content mode to keep image proportions and fill the button
+        /// assign same taget function to all 3 control buttons
+        for button in controlButtonsArray {
             button.contentMode = .scaleAspectFill
             button.setImage(#imageLiteral(resourceName: "Selected"), for: .selected)
             button.addTarget(self, action: #selector(controlButtonsAction(_:)), for: .touchUpInside)
@@ -61,12 +67,13 @@ class ViewController: UIViewController, UIGestureRecognizerDelegate {
     }
     
     
-    
     // MARK: - Gesture
     
+    /// Gesture Recognizer setup
     private func gestureSetup() {
         gestureSwipeRecognizer.delegate = self
         
+        /// Assign gesture handler to 2 case .up and .left
         let directions: [UISwipeGestureRecognizer.Direction] = [.up, .left]
         for direction in directions {
             gestureSwipeRecognizer = UISwipeGestureRecognizer(target: self, action: #selector(self.handleGesture(gesture:)))
@@ -77,31 +84,45 @@ class ViewController: UIViewController, UIGestureRecognizerDelegate {
     }
     
     
-    
+    /// Handles the swipe gesture recognizer
+    /// - Parameter gesture: pass in swipe gesture recognizer
     @objc func handleGesture(gesture: UISwipeGestureRecognizer) {
-        
-        let up = CGAffineTransform(translationX: 0, y: -UIScreen.main.bounds.height)
-        let left = CGAffineTransform(translationX: -view.bounds.width, y: 0)
-        
+        ///
+       
         if self.imageGridComplete() {
             if gesture.direction == .up && UIDevice.current.orientation.isPortrait {
-                gridViewAnimateOut(up)
+                gridViewAnimateOut(for: .up)
             } else if gesture.direction == .left && UIDevice.current.orientation.isLandscape {
-                gridViewAnimateOut(left)
+                gridViewAnimateOut(for: .left)
             }
         } else {
+            
+            /// TO_DO: Add Alert View with single Ok button
             print("Not finished")
         }
     }
     
-    func gridViewAnimateOut(_ directtion:CGAffineTransform) {
+    
+    
+    // MARK: - UI Animation
+    
+    /// Animate the girdView out of the view
+    /// - Parameter direction: pass in gesture recognizer direction
+    func gridViewAnimateOut(for direction: UISwipeGestureRecognizer.Direction ) {
+        
+        let up = CGAffineTransform(translationX: 0, y: -UIScreen.main.bounds.height)
+        let left = CGAffineTransform(translationX: -view.bounds.width, y: 0)
+        
         UIView.animate(withDuration: 0.3) {
-            self.imageGridContainerView.transform = directtion
+            /// Check which direction is passed in and assign proper up or left translation
+            self.imageGridContainerView.transform = direction == .up ? up : left
         } completion: { _ in
-            self.shareCollage()
+            /// On completion share the gridView
+            self.shareGridView()
         }
     }
     
+    /// Animate gridView back to orginial position
     func gridViewAnimateIn() {
         UIView.animate(withDuration: 0.3) {
             self.imageGridContainerView.transform = .identity
@@ -109,7 +130,8 @@ class ViewController: UIViewController, UIGestureRecognizerDelegate {
     }
     
     
-    @objc func directionChange() {
+    /// Update the UI depending on device orientation
+    @objc func updateUiOnOrientationChange() {
         if UIDevice.current.orientation.isLandscape {
             swipeIcon.image = #imageLiteral(resourceName: "Arrow Left")
             swipeLabel.text = "Swipe left to share"
@@ -122,22 +144,32 @@ class ViewController: UIViewController, UIGestureRecognizerDelegate {
     
     // MARK: - Buttons Action
     
+    /// Control the grid by hidding or showing button within the 2 stackviews
+    /// - Parameter sender: pass in the control button tapped
     @objc private func controlButtonsAction(_ sender: UIButton) {
         
-        for button in controlButtons {
+        /// for loop to reset all control button in the collection to non selected state
+        for button in controlButtonsArray {
             button.isSelected = false
         }
-        controlButtons[sender.tag].isSelected = true
+        /// set sender button to selected stated by using button tag property
+        controlButtonsArray[sender.tag].isSelected = true
         
-        for button in imageButtons {
+       /// for loop to reset all imageButton in the collection to non hiden state
+        for button in imageButtonsArray {
             button.isHidden = false
         }
-        
+        /// To match the layout Icons of the control button
+        /// image buttons are hidden depending on which control button is tapped
+        /// there are 3 cases:    top right is hidden  (imageButton tag 1)
+        ///                bottom right is hidden  (imageButton tag 3)
+        ///                none of the button are hidden then all 4 button are showing
+        /// by hidding those 2 button , the stack view streches the adjacent button to full width
         switch sender.tag {
         case 0:
-            imageButtons[1].isHidden = true
+            imageButtonsArray[1].isHidden = true
         case 1:
-            imageButtons[3].isHidden = true
+            imageButtonsArray[3].isHidden = true
         default:
             return
         }
@@ -145,30 +177,48 @@ class ViewController: UIViewController, UIGestureRecognizerDelegate {
     }
     
     
+    /// Presents the image picker whe, an image button is tapped
+    /// - Parameter sender: pass in the image button tapped
     @objc func imageButtonAction(_ sender: UIButton) {
+        /// the tappedImageButtonId var keep tracked of the  tag for the button tapped
+        /// so the image is assigned to the proper button
         tappedImageButtonId = sender.tag
-        imagePicker?.present(from: imageButtons[tappedImageButtonId])
+        imagePicker?.present(from: imageButtonsArray[tappedImageButtonId])
     }
     
     
     // MARK: Private Functions
     
-    private func shareCollage() {
+    /// Share gridView as an image
+    private func shareGridView() {
         
-        imageGridContainerView.convertToImage { (image) in
+        /// convert uiview to image, returns an image in the closure .
+        /// assign weak self to avoid retain cycles and memory leak
+        imageGridContainerView.convertToImage { [weak self] image in
+            
+            /// pass in the image to the activity controller
             let activityController = UIActivityViewController(activityItems: [image], applicationActivities: nil)
+            
+            /// on completion or dismissal animate the grid view back to its original position
+            /// _ are put in the closure as we re not using any of these datas
             activityController.completionWithItemsHandler = { _, _, _, _ in
-                self.gridViewAnimateIn()
+                self?.gridViewAnimateIn()
             }
-            self.present(activityController, animated: true, completion: nil)
+            /// present the activity controller
+            self?.present(activityController, animated: true, completion: nil)
         }
     }
     
     
+    /// Keep track if all images are set for the given layout
+    /// - Returns: return true if all images required are present
     private func imageGridComplete() -> Bool {
+        
         var availableImageCount = 0
         var imageToSetCount = 0
         
+        /// for loop counting how many images are not hidden in the top image stackview
+        ///
         for view in topImageStackView.arrangedSubviews {
             if !view.isHidden {
                 availableImageCount += 1
@@ -197,9 +247,13 @@ class ViewController: UIViewController, UIGestureRecognizerDelegate {
 
 extension ViewController: ImagePickerDelegate {
     
+    /// imagepicker selected image
+    /// - Parameter image: optional returned from the image picker
     func didSelect(image: UIImage?) {
+        /// unwraped the optional to use the iamge if not nil
         guard let image = image else {return}
-        imageButtons[tappedImageButtonId].setImage(image, for: .normal)
+        /// assigned the image to the button by using the tappedImageButtonId var that keeps track of the button tag pproperty
+        imageButtonsArray[tappedImageButtonId].setImage(image, for: .normal)
     }
     
 }
